8151e12b443c11c48f52da2d788514d21cf1cb30
ASHALATHA KOYYANA <ashakoyyana1802@gmail.com>
2025-12-31 10:49:55 +0530

    Update test_contacts.py

--- week3-contact-manager/test_contacts.py
+++ week3-contact-manager/test_contacts.py
@@ -1,1 +1,13 @@
+from contacts_manager import validate_phone, validate_email
+
+def test_phone():
+    print(validate_phone("9876543210"))
+    print(validate_phone("123"))
+
+def test_email():
+    print(validate_email("test@gmail.com"))
+    print(validate_email("wrongemail"))
+
+test_phone()
+test_email()
 
